76d5655ceb7892efc3e3609eddf397875ca9adcf
felkinsio <143757815+felkinsio@users.noreply.github.com>
2026-04-14 10:54:29 -0500

    Add author comments to Algoritmos Caminos más cortos.py
    Added authors' comments to the top of the script.

--- Algoritmos ciencias/Algoritmos Caminos más cortos.py
+++ Algoritmos ciencias/Algoritmos Caminos más cortos.py
@@ -1,3 +1,7 @@
+#Kevin Daniel Agudelo Sotelo - 20222020125 
+#Santiago Alejandro Guada Bohorquez - 20231020182 
+#Johan Felipe Pinzon Garcia - 20222020176 
+#Andres felipe correa mendez - 20221020141
 
 import time
 import random
